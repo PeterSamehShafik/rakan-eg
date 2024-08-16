2ffe2e3607bed14cd23e32f4a6cc790150c93bf6
PeterSamehShafik <petosameh82@gmail.com>
2024-08-16 17:11:20 +0300

    caruosel & footer

--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -4,15 +4,19 @@ import Navbar from "../Components/Navbar/Navbar";
 import TopLoader from "../Components/TopLoader";
 import Home from "../Pages/Home";
 import NotFound from "../Pages/NotFound";
+import About from "../Pages/About.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Navbar />
     <TopLoader>
       <Route path="" element={<Home />} />
+      <Route path="/about-us" element={<About />} />
       <Route path="404" element={<NotFound />} />
       <Route path="*" element={<Navigate to="/404" />} />
     </TopLoader>
+    <Footer />
   </BrowserRouter>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+      keyframes: {
+        slideInFromBottom: {
+          '0%': {
+            transform: 'translateY(100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        slideInFromBottom: 'slideInFromBottom 0.6s ease-out',
+      },
+    },
   },
   plugins: [],
 };
